00df1d1e78f2ca4eb5917cf84075536d6cf50af8
Ronaldosdeltas <ronaldosssantos1@gmail.com>
2025-07-19 16:52:38 -0300

    creatin the project interphase

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import { useState } from 'react'
 import './App.css'
-
+import img from './assets/logo.png'
 function App() {
 
  
   return (
     <>
     
-      <div>
-            
-            
+      <div className='container'>
+        <img
+          src={img}
+          alt="logo"
+          className='logo'
+        />
+        <h2 className='title'>categories</h2>
+        <section className='categories'>
+          <button className='category-bt'>Motivation</button>
+          <button className='category-bt'>Good Day</button> 
+
+        </section>
+            <button className='btn'>Best Phrase</button>
+            <h2 className='bestPhrases'> the Phrases</h2>
     </div>
     </>
   )
